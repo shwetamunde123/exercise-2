fad387306bf74ae2e2def9209d464de0b74a143a
shwetamunde123 <89522061+shwetamunde123@users.noreply.github.com>
2021-08-27 23:55:00 +0530

    Add files via upload

---
+++ ex 14_sum of series.java
@@ -0,0 +1,16 @@
+//Write a program to calculate the sum of following series where n is input by user.1 + 1/2 + 1/3 + 1/4 + 1/5 +…………1/n
+
+import java.util.Scanner;
+public class SumOfSeries {
+	public static void main(String[] args) {
+		Scanner console = new Scanner(System.in);
+		int number;
+		double sum = 0;
+		System.out.print("Enter number of terms of series:");
+		number = console.nextInt();
+		for (int i = 1; i <= number; i++) {
+			sum += 1.0 / i;
+		}
+		System.out.println("sum:" + sum);
+	}
+}
