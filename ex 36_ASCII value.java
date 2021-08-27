10f27d1e559ba642d8eddb5b2da4f9fb9bdae6df
shwetamunde123 <89522061+shwetamunde123@users.noreply.github.com>
2021-08-28 00:34:28 +0530

    Add files via upload

---
+++ ex 36_ASCII value.java
@@ -0,0 +1,15 @@
+//Java Program to Find ASCII Value of a Character
+
+public class AsciiValue {
+
+    public static void main(String[] args) {
+
+        char ch = 'a';
+        int ascii = ch;
+        // You can also cast char to int
+        int castAscii = (int) ch;
+
+        System.out.println("The ASCII value of " + ch + " is: " + ascii);
+        System.out.println("The ASCII value of " + ch + " is: " + castAscii);
+    }
+}
