19e8b14390c0967f8037a9b646b2c7060a2d1cfa
shwetamunde123 <89522061+shwetamunde123@users.noreply.github.com>
2021-08-28 00:34:09 +0530

    Add files via upload

---
+++ ex 35_inc_dec.java
@@ -0,0 +1,15 @@
+//Increment ++ and Decrement -- Operator Overloading in Java Programming
+
+class Operator {
+    public static void main(String[] args) {
+        int var1 = 5, var2 = 5;
+
+        // 5 is displayed
+        // Then, var1 is increased to 6.
+        System.out.println(var1++);
+
+        // var2 is increased to 6
+        // Then, var2 is displayed
+        System.out.println(++var2);
+    }
+}
