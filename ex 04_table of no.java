59ff529420b46b69131796fd7c9da65dfed0b4c6
shwetamunde123 <89522061+shwetamunde123@users.noreply.github.com>
2021-08-27 23:50:34 +0530

    Add files via upload

---
+++ ex 04_table of no.java
@@ -0,0 +1,14 @@
+//Write a program that prompts the user to input a positive integer. It should then print the multiplication table of that number.
+
+
+import java.util.Scanner;
+public class MultiplicationTable {
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		System.out.print("Input a number:");
+		int num1 = in.nextInt();
+		for (int i = 0; i < 10; i++) {
+			System.out.println(num1 + " x " + (i + 1) + " = " + (num1 * (i + 1)));
+		}
+	}
+}
