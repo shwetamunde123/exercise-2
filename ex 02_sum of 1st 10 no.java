ad13974cf18d210e20a29d60563b1bbab8b2e75b
shwetamunde123 <89522061+shwetamunde123@users.noreply.github.com>
2021-08-27 23:48:35 +0530

    Add files via upload

---
+++ ex 02_sum of 1st 10 no.java
@@ -0,0 +1,11 @@
+
+
+public class Sum_Of_First_10_Natural_Numbers {
+	public static void main(String[] args)   {
+		int i, num = 10, sum = 0;
+		for (i = 1; i <= num; ++i)  {
+			sum = sum + i;
+		}
+		System.out.println("Sum of First 10 Natural Numbers is = " + sum);
+	}
+}
