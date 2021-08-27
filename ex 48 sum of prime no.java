95a00a10e6a5f0d9fc6c5dd25d36b1d2a4a9a906
shwetamunde123 <89522061+shwetamunde123@users.noreply.github.com>
2021-08-28 00:38:58 +0530

    Add files via upload

---
+++ ex 48 sum of prime no.java
@@ -0,0 +1,40 @@
+//Java Program to Check Whether a Number can be Express as Sum of Two Prime Numbers
+public class  PMain {
+
+  public static void main(String[] args) {
+    int number = 34;
+    boolean flag = false;
+    for (int i = 2; i <= number / 2; ++i) {
+
+      
+      if (checkPrime(i)) {
+
+        
+        if (checkPrime(number - i)) {
+
+          
+          System.out.printf("%d = %d + %d\n", number, i, number - i);
+          flag = true;
+        }
+
+      }
+    }
+
+    if (!flag)
+      System.out.println(number + " cannot be expressed as the sum of two prime numbers.");
+  }
+
+  
+  static boolean checkPrime(int num) {
+    boolean isPrime = true;
+
+    for (int i = 2; i <= num / 2; ++i) {
+      if (num % i == 0) {
+        isPrime = false;
+        break;
+      }
+    }
+
+    return isPrime;
+  }
+}
