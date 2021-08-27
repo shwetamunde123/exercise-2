11ba3c834d80623c06074749ed92f6dabbf737c4
shwetamunde123 <89522061+shwetamunde123@users.noreply.github.com>
2021-08-28 01:21:06 +0530

    Add files via upload

---
+++ ex 73 sort in descending.java
@@ -0,0 +1,29 @@
+//Write a JAVA program to read seven numbers and sorts them in descending order.
+
+import java.util.Scanner;
+public class SortInDescendingOrder {
+    public static void main(String args[]) {
+        Scanner in = new Scanner(System.in);
+        int n = 7;
+        int arr[] = new int[n];
+        
+        System.out.println("Enter the seven numbers:");
+        for (int i = 0; i < n; i++) {
+            arr[i] = in.nextInt();
+        }
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = 0; j < n - i - 1; j++) {
+                if (arr[j] < arr[j + 1]) {
+                    int t = arr[j];
+                    arr[j] = arr[j+1];
+                    arr[j+1] = t;
+                }
+            } 
+        }
+        
+        System.out.println("Sorted in descending order:");
+        for (int i = 0; i < n; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+}
