20ce0118fbea8b72599330fac1a37124fddd09cb
shwetamunde123 <89522061+shwetamunde123@users.noreply.github.com>
2021-08-28 00:29:15 +0530

    Add files via upload

---
+++ ex 21_even or odd.java
@@ -0,0 +1,18 @@
+//Java Program to Check Whether Number is Even or Odd
+import java.util.Scanner;
+
+public class EvenOdd {
+
+    public static void main(String[] args) {
+
+        Scanner reader = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        int num = reader.nextInt();
+
+        if(num % 2 == 0)
+            System.out.println(num + " is even");
+        else
+            System.out.println(num + " is odd");
+    }
+}
