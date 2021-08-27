efcf970cad0107dfeff8d3da3df686f39d119bc9
shwetamunde123 <89522061+shwetamunde123@users.noreply.github.com>
2021-08-28 01:16:09 +0530

    Add files via upload

---
+++ ex 65 no of vowels ,consonant ,digits ,white spaces.java
@@ -0,0 +1,39 @@
+//Java Program to Find the Number of Vowels, Consonants, Digits and White Spaces in a String
+
+class Main {
+
+  public static void main(String[] args) {
+    String line = "Government Residential Women's Polytechnic College";
+    int vowels = 0, consonants = 0, digits = 0, spaces = 0;
+
+    line = line.toLowerCase();
+    for (int i = 0; i < line.length(); ++i) {
+      char ch = line.charAt(i);
+
+     
+      if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u') {
+        ++vowels;
+      }
+
+    
+      else if ((ch >= 'a' && ch <= 'z')) {
+        ++consonants;
+      }
+      
+      
+      else if (ch >= '0' && ch <= '9') {
+        ++digits;
+      }
+      
+      
+      else if (ch == ' ') {
+        ++spaces;
+      }
+    }
+
+    System.out.println("Vowels: " + vowels);
+    System.out.println("Consonants: " + consonants);
+    System.out.println("Digits: " + digits);
+    System.out.println("White spaces: " + spaces);
+  }
+}
