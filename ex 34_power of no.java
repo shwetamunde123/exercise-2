3b21746fd70e1aadab7efc718ee2871934e4070b
shwetamunde123 <89522061+shwetamunde123@users.noreply.github.com>
2021-08-28 00:33:52 +0530

    Add files via upload

---
+++ ex 34_power of no.java
@@ -0,0 +1,16 @@
+//Java Program to Calculate Power of a Number
+class Main {
+  public static void main(String[] args) {
+
+    int base = 3, exponent = 4;
+
+    long result = 1;
+
+    while (exponent != 0) {
+      result *= base;
+      --exponent;
+    }
+
+    System.out.println("Answer = " + result);
+  }
+}
