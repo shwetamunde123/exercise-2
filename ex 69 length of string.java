9c2a12c4007d484c17ddaced6a919e17f9b494e0
shwetamunde123 <89522061+shwetamunde123@users.noreply.github.com>
2021-08-28 01:17:55 +0530

    Add files via upload

---
+++ ex 69 length of string.java
@@ -0,0 +1,8 @@
+//Java Program to Find the Length of a String
+public class LengthExample{  
+public static void main(String args[]){  
+String s="Government";  
+
+System.out.println("string length is: "+s.length());
+}
+}  
