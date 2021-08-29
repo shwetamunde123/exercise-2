dbee77f3d35b0b680f9d6b8cc034f8121afeb28f
shwetamunde123 <89522061+shwetamunde123@users.noreply.github.com>
2021-08-29 23:54:14 +0530

    Add files via upload

---
+++ ex 81_duplicate character.java
@@ -0,0 +1,38 @@
+//Write a program to remove all the duplicate characters from a given input String, like, if the given String is "Java" then the output should be "Java". The second or further occurrence of duplicates should be removed.
+
+import java.util.*;   
+   class RemoveDuplicationsFromString
+{   
+  static void removeDuplicate(char str[], int length)   
+    {   
+      int index = 0;   
+   for (int i = 0; i < length; i++)   
+        {   
+              int j;   
+            for (j = 0; j < i; j++)    
+            {   
+                if (str[i] == str[j])   
+                {   
+                    break;   
+                }   
+            }   
+   if (j == i)    
+            {   
+                str[index++] = str[i];   
+            }   
+        }   
+        System.out.println(String.valueOf(Arrays.copyOf(str, index)));   
+    }   
+    
+   
+    public static void main(String[] args)   
+    {   
+        String info = "WomensPolytechnicLatur";  
+        
+        char str[] = info.toCharArray();  
+       
+        int len = str.length;   
+        
+        removeDuplicate(str, len);   
+    }   
+}
